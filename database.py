da666e33c276ceb8a84df3923593cc735d49abc1
Shumba Ashley <ashleytshumba@gmail.com>
2022-07-01 01:01:11 +0200

    contacts to atlas

--- database.py
+++ database.py
@@ -4,7 +4,11 @@
 import motor.motor_asyncio
 from model import Employee
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+
+URI = "mongodb+srv://threetiersystem:Xinyxo1DIUotsoEp@cluster0.ie2a2.mongodb.net/?retryWrites=true&w=majority"
+# URI ='mongodb://localhost:27017/'
+
+client = motor.motor_asyncio.AsyncIOMotorClient(URI)
 database = client.face_db
 collection = database.face
 
